25bedd7590b2406b3d106d4dc405d1add5afab8c
Pavel Grigorov <pkgrigorov@uni-sofia.bg>
2020-10-27 11:47:35 +0200

    Added doctest to RPN

--- group1/Pavel_82038/RPN/RPN.cpp
+++ group1/Pavel_82038/RPN/RPN.cpp
@@ -1,12 +1,16 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <stack>
 #include <assert.h>
 #include <math.h>
+#include "../../../shared/specs/doctest.h"
 
 //Convert infix expression to
 //Reverse Polish Notation
+
 
 bool isOperand(const char c)
 {
@@ -173,19 +177,26 @@ int calculateRPN(const std::string &expression)
     return numbers.top();;
 }
 
+
+TEST_CASE("Conversion test")
+{
+    CHECK(convertExpression("2^3") == "23^");
+    CHECK(convertExpression("5+7+1+2") == "57+1+2+");
+    CHECK(convertExpression("(5+(7+(1+2)))") == "5712+++");
+    CHECK(convertExpression("((1+2)*3/(1+2))") == "12+3*12+/");
+}
+
+TEST_CASE("Calculation test")
+{
+    CHECK(calculateRPN(convertExpression("2^3")) == 8);
+    CHECK(calculateRPN(convertExpression("5+7+1+2")) == 15);
+    CHECK(calculateRPN(convertExpression("(5+(7+(1+2)))")) == 15);
+    CHECK(calculateRPN(convertExpression("((1+2)*3/(1+2))")) == 3);
+}
+
 int main()
 {
-    std::cout << convertExpression("2^3");
-    std::cout << '\n';
-    std::cout << calculateRPN(convertExpression("2^3"));
-    std::cout << '\n';
-    std::cout << convertExpression("5+7+1+2");
-    std::cout << '\n';
-    std::cout << calculateRPN(convertExpression("5+7+1+2"));
-    std::cout << '\n';
-    std::cout << convertExpression("((1+2)*3/(1+2))");
-    std::cout << '\n';
-    std::cout << calculateRPN(convertExpression("((1+2)*3/(1+2))"));
+    doctest::Context().run(); 
 
     return 0;
 }
